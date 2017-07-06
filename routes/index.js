0cfd3a0df987e72d8f1772a1be28a349820cffc1
erwinwahyura <you@example.com>
2017-07-06 10:44:52 +0700

    routes api collection foods done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express')
-
 var router = express.Router()
+var c_food = require('../controllers/c_food.js')
 
 router.get('/', function (req, res) {
   res.send('Hi Erwin')
 })
+
+router.get('/api/foods', c_food.getAll)
+router.post('/api/foods', c_food.add)
+router.get('/api/foods/:_id', c_food.getById)
+router.put('/api/foods/:_id', c_food.edit)
+router.delete('/api/foods/:_id', c_food.remove)
 
 module.exports = router
