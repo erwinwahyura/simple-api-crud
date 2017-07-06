b2d63817c3060c987c7684184bdf4438411ce89f
erwinwahyura <you@example.com>
2017-07-06 10:16:38 +0700

    initialize mongoose connection for firsttime run app.js

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/simple-crud-erwin');
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello Erwin')
-})
+var index = require('./routes/index')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('koneksi sukses');
+});
+
+app.use('/', index)
+// app.get('/', function (req, res) {
+//   res.send('Hello Erwin')
+// })
 
 app.listen(3000)
